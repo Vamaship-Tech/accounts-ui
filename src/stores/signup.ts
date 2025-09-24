04faa8f855042fd40b06454f64c125b3d9d7b3b7
amansinghl <aman.singh@vamaship.com>
2025-09-24 16:39:23 +0530

    fix

--- src/stores/signup.ts
+++ src/stores/signup.ts
@@ -218,7 +218,7 @@ export const useSignupStore = defineStore('signup', () => {
       })
       console.log(result, 'result')
       console.log(result.result, 'result.result != 0')
-      if (result.result != 0) return { success: false }
+      if (result.result == 0) return { success: false }
 
       // Save mobile session
       SessionManager.saveMobileSession(formData.value.phone, formData.value.countryCode)
